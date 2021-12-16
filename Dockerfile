e051fd6f5444fec85c0d4fbbf81f31aa06531f08
Yoelvis Oses Sosa <Yoelvis.Oses@bcbsnc.com>
2021-12-16 10:20:45 -0500

    Challenge NOYO - Yoelvis Oses

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM python:3.9.2
 RUN pip install --upgrade pip
 
 COPY requirements.txt .
-RUN pip install -r ./requirements.txt --no-user
+RUN pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -r ./requirements.txt --no-user
 
 CMD ["flask", "run"]
--- service/api/addresses.py
+++ service/api/addresses.py
@@ -1,6 +1,6 @@
 import logging
 
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
 from flask import abort, jsonify
 from webargs.flaskparser import use_args
@@ -49,28 +49,33 @@ def create_address(payload, person_id):
     person = Person.query.get(person_id)
     if person is None:
         abort(404, description="person does not exist")
-    # If there are no AddressSegment records present for the person, we can go
-    # ahead and create with no additional logic.
-    elif len(person.address_segments) == 0:
-        address_segment = AddressSegment(
-            street_one=payload.get("street_one"),
-            street_two=payload.get("street_two"),
-            city=payload.get("city"),
-            state=payload.get("state"),
-            zip_code=payload.get("zip_code"),
-            start_date=payload.get("start_date"),
-            person_id=person_id,
-        )
+    # If there are AddressSegment records present for the person, we can check dates.
+    elif len(person.address_segments) > 0:        
 
-        db.session.add(address_segment)
-        db.session.commit()
-        db.session.refresh(address_segment)
-    else:
-        # TODO: Implementation
-        # If there are one or more existing AddressSegments, create a new AddressSegment
-        # that begins on the start_date provided in the API request and continues
-        # into the future. If the start_date provided is not greater than most recent
+        # If the start_date provided is not greater than most recent
         # address segment start_date, raise an Exception.
-        raise NotImplementedError()
+        last_address = person.address_segments[-1]
+        if last_address.start_date > payload.get("start_date"):
+            abort(404, description="The start date for this new segment must be greater than most recent address")
+        else:
+            # Seting end_date to the start date for new address
+            last_address.end_date = payload.get("start_date")
+            db.session.commit()
+    # If there are no existing AddressSegments or new one is after more recent one, create a new AddressSegment
+    # that begins on the start_date provided in the API request and continues into the future.  
+        
+    address_segment = AddressSegment(
+        street_one=payload.get("street_one"),
+        street_two=payload.get("street_two"),
+        city=payload.get("city"),
+        state=payload.get("state"),
+        zip_code=payload.get("zip_code"),
+        start_date=payload.get("start_date"),
+        person_id=person_id,
+    )
+
+    db.session.add(address_segment)
+    db.session.commit()
+    db.session.refresh(address_segment)
 
     return jsonify(AddressSchema().dump(address_segment))
